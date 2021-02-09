0bcc6403cd57d85979db71751c67d7cf33de27af
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-02-09 17:07:17 -0500

    Update emotelist.ts
    Functions have been moved outside of run command. Instead of executing the message send inside run, this is performed in a function that can now be used other events, such as emojiCreate and emojiDelete.
    
    Command is now throttled to 5 uses within 20 seconds.

--- src/commands/emote/emotelist.ts
+++ src/commands/emote/emotelist.ts
@@ -1,134 +1,139 @@
 // emotelist.ts
-import { MessageEmbed, Guild, GuildEmoji, Message } from 'discord.js';
+import { MessageEmbed, Guild, GuildEmoji, Message, Collection } from 'discord.js';
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 
-
-const emoteMessageArray = (emotes: string[]): string[] => {
-    // Recursively call an array of emotes to return them in a multi-line message of 25 emotes, up to 5 emotes per line
-    // Base case: 5 or less emotes are given, return the emotes in a single string
-    if (emotes.length <= 5) {
-        return [emotes.join('')];
-    }
-    // Second case: 25 or less emotes are given, return the emotes in groups of five, joined by a new line
-    else if (emotes.length <= 25) {
-        return [[emoteMessageArray(emotes.slice(0, 5)), emoteMessageArray(emotes.slice(5))].join('\n')];
-    }
-    // Otherwise, concatenate the first 25 emotes with the remainder
-    else {
-        return ([] as string[]).concat(emoteMessageArray(emotes.slice(0, 25)), emoteMessageArray(emotes.slice(25)));
-    }
-};
+interface emoteMessage {
+  embed: MessageEmbed;
+  staticList: string[];
+  animatedList: string[];
+}
 
 const today = (): string => {
-    // Get the date and time when the command is executed
-    const date = new Date();
-    return date.toUTCString();
+  const date = new Date();
+  return date.toUTCString();
 };
 
+const emoteToText = (emotes: GuildEmoji[]): string[] => {
+  return emotes
+    .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
+    .map((emote) => (emote.animated ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`));
+};
+
+export const displayEmotes = (message: CommandoMessage, emotes: string[]) => {
+  let emoteLine = '';
+  for (let i = 1; i <= emotes.length; i++) {
+    emoteLine += emotes[i - 1];
+
+    if (i % 5 == 0 || i == emotes.length) {
+      message.say(emoteLine);
+      emoteLine = '';
+    }
+  }
+};
+
+export const getGuildEmotes = async (guild: Guild): Promise<emoteMessage> => {
+  const emoteStatic = guild.emojis.cache.filter((emote) => !emote.animated).map((emote) => emote);
+  const emoteAnimated = guild.emojis.cache.filter((emote) => emote.animated).map((emote) => emote);
+  // Create the embed message as the header
+  const embedMessage = new MessageEmbed()
+    .setTitle(`${guild.name} Emote List`)
+    .setThumbnail(guild.iconURL() as string)
+    .setFooter(`Updated ${today()}`)
+    .addFields([
+      { name: 'Static Emotes', value: `(${emoteStatic.length}/100)`, inline: true },
+      { name: 'Animated Emotes', value: `(${emoteAnimated.length}/100)`, inline: true },
+    ]);
+
+  // Create the emoteMessage object
+  const message: emoteMessage = {
+    embed: embedMessage,
+    staticList: emoteToText(emoteStatic),
+    animatedList: emoteToText(emoteAnimated),
+  };
+
+  return message;
+};
+
+export const sendGuildEmotes = async (message: CommandoMessage, emoteMsg: emoteMessage): Promise<void> => {
+  await message.say(emoteMsg.embed);
+  await message.say('**Static Emotes**');
+  displayEmotes(message, emoteMsg.staticList);
+  await message.say('**Animated Emotes**');
+  displayEmotes(message, emoteMsg.animatedList);
+};
 
 export default class serverEmoteList extends Command {
-    constructor(client: CommandoClient) {
-        super(client, {
-            name: 'emotelist',
-            group: 'emote',
-            memberName: 'emotelist',
-            description: 'Sends the list of emotes from a server. The author can only show emotes from servers they share with the bot. Send \'0\' to show the list of emotes in the current server.',
-            examples: ['+emotelist', '+emotelist 0'],
-            guildOnly: true,
-            clientPermissions: ['SEND_MESSAGES'],
-            userPermissions: ['MANAGE_EMOJIS', 'MANAGE_MESSAGES'],
-        });
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'emotelist',
+      group: 'emote',
+      memberName: 'emotelist',
+      description:
+        "Sends the list of emotes from a server. The author can only show emotes from servers they share with the client. Send '0' to show the list of emotes in the current server.",
+      examples: ['+emotelist', '+emotelist 0'],
+      guildOnly: true,
+      clientPermissions: ['SEND_MESSAGES'],
+      userPermissions: ['MANAGE_EMOJIS', 'MANAGE_MESSAGES'],
+      throttling: {
+        usages: 5,
+        duration: 20,
+      },
+    });
+  }
+
+  async run(message: CommandoMessage) {
+    // remove the command from the message contents
+    const arg = message.content.slice(this.examples[0].length).trim();
+    if (arg === '0') {
+      // else if the user input 0 as the arg, use the current guild
+      const emoteMsg = await getGuildEmotes(message.guild as Guild);
+      await sendGuildEmotes(message, emoteMsg);
+      return message;
     }
-
-    async run(message: CommandoMessage): Promise<null> {
-        // return early if author is bot
-        if (message.author.bot) return null;
-
-        // if the user entered '0', send the emotes for the messaged server
-        // else ask the users to select from a server they share with the bot
-        const args = message.content.split(/ +/);
-        if (args[args.length - 1] === '0') {
-            const guild = message.guild as Guild;
-            sendGuildEmotes(guild);
+    const userGuilds = message.client.guilds.cache
+      .filter((guild) => guild.member(message.author.id) != null)
+      .sort((guildA, guildB) => {
+        if (
+          guildA.name.toLowerCase() == message.guild?.name.toLowerCase() ||
+          guildA.name.toLowerCase() < guildB.name.toLowerCase()
+        ) {
+          return -1;
         }
-        else {
-            // get the array of servers that the author shares with the client
-            // then sort servers by name, while listing the messaged server on top
-            const userGuilds = message.client.guilds.cache.filter((guild: any) => {
-                return guild.members.fetch(message.author.id)
-                    .then(() => guild)
-                    .catch(() => null);
-            }).map(guild => guild)
-                .sort((a, b) => {
-                    return (a.name.toLowerCase() < b.name.toLowerCase()) ? -1 : 1;
-                }).sort(a => {
-                    return (a.name.toLowerCase() === (message.guild as Guild).name.toLowerCase()) ? -1 : 1;
-                });
+        if (guildA.name.toLowerCase() > guildB.name.toLowerCase()) return 1;
+        return 0;
+      })
+      .map((guild) => guild);
 
-            // ask the user to choose the server to display emotes for
-            const guildNames = userGuilds.map((guild, index) => `${index}: ${guild.name}`);
-            message.reply(`choose the server number below to display the emotes from. Send \`cancel\` to abort this command.\n\`\`\`\n${guildNames.join('\n')}\n\`\`\``).then(msg => msg.delete({ timeout: 20 * 1000 }));
+    const guildNames = userGuilds.map((guild, index) => `${index}: ${guild.name}`).join('\n');
+    const prompt = 'choose a number below:\n```\n' + guildNames + '```\n or `cancel` to abort this command.\n';
+    (await message.reply(prompt)).delete({ timeout: 30 * 1000 });
 
-            const filter = (msg: Message) => (msg.author.id === message.author.id) && (msg.content.toLowerCase() === 'end' || (parseInt(msg.content, 10) >= 0 && parseInt(msg.content, 10) < guildNames.length));
-            const collector = message.channel.createMessageCollector(filter, { time: 30 * 1000 });
+    const filter = (msg: Message) =>
+      msg.author.id === message.author.id &&
+      (msg.content.toLowerCase() === 'cancel' ||
+        (parseInt(msg.content, 10) >= 0 && parseInt(msg.content, 10) < userGuilds.length));
 
-            collector.on('collect', collect => {
-                // ends the collector when a value index is given or when 'end' is given
-                if (collect.content.toLowerCase() === 'end') collector.stop();
-                if (userGuilds[collect.content]) {
-                    sendGuildEmotes(userGuilds[collect.content]);
-                    collector.stop();
-                }
-            });
+    const collector = message.channel.createMessageCollector(filter, { time: 30 * 1000 });
 
-            collector.on('end', collected => {
-                // delete all collected messages
-                collected.forEach(msg => msg.delete({ timeout: 2000 }));
-            });
-        }
+    collector.on('collect', async (collect: Message) => {
+      if (collect.content.toLowerCase() === 'end') return collector.stop();
 
+      const guild = userGuilds[parseInt(collect.content, 10)];
+      if (guild) {
+        const emoteMsg = await getGuildEmotes(guild);
+        await sendGuildEmotes(message, emoteMsg);
+        collector.stop();
+      }
+    });
 
-        function sendGuildEmotes(guild: Guild) {
-            // send the list of emotes from the specified guild object
-            const emotes = guild.emojis.cache;
-            const emoteFormat = (emote: GuildEmoji): string => {
-                return (emote.animated) ? `<a:${emote.name}:${emote.id}>` : `<:${emote.name}:${emote.id}>`;
-            };
-            const emoteSort = (a: GuildEmoji, b: GuildEmoji): number => {
-                return (a.name.toLowerCase() < b.name.toLowerCase()) ? -1 : 1;
-            };
+    collector.on('end', async (collected: Collection<string, Message>) => {
+      if (collected.some((collect) => collect.content.toLowerCase() === 'cancel')) {
+        (await message.reply('canceling the command.')).delete({ timeout: 5000 });
+      }
 
-            const emoteStatic = emotes.filter(emote => !emote.animated).sort((a, b) => emoteSort(a, b));
-            const emoteAnimated = emotes.filter(emote => emote.animated).sort((a, b) => emoteSort(a, b));
+      collected.forEach((msg) => msg.delete({ timeout: 2000 }));
+    });
 
-            const textStatic = emoteMessageArray(emoteStatic.map(emote => emoteFormat(emote)));
-            const textAnimated = emoteMessageArray(emoteAnimated.map(emote => emoteFormat(emote)));
-
-            // send the emotes into the messaged channel
-            const embedMessage = new MessageEmbed()
-                .setTitle(`${guild.name} Emote List`)
-                .setThumbnail(guild.iconURL() as string)
-                .setFooter(`Updated ${today()}`)
-                .addField('Static Emotes', `(${emoteStatic.size}/100)`, true)
-                .addField('Animated Emotes', `(${emoteAnimated.size}/100)`, true);
-
-            // message.say(text)))
-            message.say(embedMessage)
-                .then(() => message.say('**Static Emotes**'))
-                .then(() => {
-                    textStatic.forEach(text => {
-                        if (text !== '') message.say(text);
-                    });
-                })
-                .then(() => message.say('**Animated Emotes**'))
-                .then(() => {
-                    textAnimated.forEach(text => {
-                        if (text !== '') message.say(text);
-                    });
-                });
-        }
-
-
-        return null;
-    }
+    return null;
+  }
 }
