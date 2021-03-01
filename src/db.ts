44146f2d88f0f1cb4853f267d709d011462a99c2
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-02-28 20:17:09 -0500

    Initial upload for typeorm / mysql database
    mysql database to be manipulated with typeorm. Reference files updated.
    
    db.ts exports a function, prepareConnection(), that closes any existing database connections prior to connecting to a database for use in any other file.
    
    ready.ts contains a possible block where client guilds are added to a table, if they do not already exist in the database.

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+import { createConnection, getConnection } from 'typeorm';
+
+config({ path: join(__dirname, '../process.env') });
+
+let connectionReadyPromise: Promise<void> | null = null;
+
+export const prepareConnection = async () => {
+  if (!connectionReadyPromise) {
+    connectionReadyPromise = (async () => {
+      try {
+        const staleConnection = getConnection();
+        await staleConnection.close();
+      } catch (e) {
+        console.log('No stale connection to clean up.');
+      }
+      await createConnection();
+    })();
+  }
+  return connectionReadyPromise;
+};
