20cf95855440bb54743f49d5af57a772b06ca3c3
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-03-01 13:01:55 -0500

    bugfix on message deletion in message event
    message.deleted() returns false despite being deleted prior in code & being fetched, thus throwing errors when attempting to delete the message down the code.
    
    Message deletion logic is separated --- original messages for non-nitro emoji commands are always deleted. The non-nitro webhook is checked against the spam channel and is deleted & logged if invalid. The function then ends, preventing message deletion checks down the code.
    
    Any remaining messages are also checked against the spam channel and are deleted & logged as necessary.

--- src/events/message.ts
+++ src/events/message.ts
@@ -11,22 +11,21 @@ export default async (client: CommandoClient, message: CommandoMessage) => {
   if (message.channel.type === 'dm') return;
 
   const nonNitroWebhookMessage = await handleNonNitroEmoji(message);
+  if (nonNitroWebhookMessage != null) {
+    message.delete().catch((e) => console.log('Failed to delete a message: ', e));
+
+    const botSpamEmojiDeleteEmbed = await handleSpamEmojiChannels(nonNitroWebhookMessage);
+    if (botSpamEmojiDeleteEmbed != null) {
+      nonNitroWebhookMessage.delete().catch((e) => console.log('Failed to delete a message: ', e));
+      sendDeleteLog(message, botSpamEmojiDeleteEmbed);
+    }
+    return;
+  }
+
   const spamEmojiDeleteEmbed = await handleSpamEmojiChannels(message);
   if (spamEmojiDeleteEmbed != null) {
-    if (message.deleted == false) message.delete().catch((e) => console.log('Failed to delete a message: ', e));
-    if (nonNitroWebhookMessage == null) {
-      sendDeleteLog(message, spamEmojiDeleteEmbed);
-    } else {
-      const botSpamEmojiDeleted = await handleSpamEmojiChannels(nonNitroWebhookMessage);
-      if (botSpamEmojiDeleted != null) {
-        if (nonNitroWebhookMessage.deleted == false) {
-          nonNitroWebhookMessage.delete().catch((e) => console.log('Failed to delete a message: ', e));
-        }
-        sendDeleteLog(message, spamEmojiDeleteEmbed);
-      }
-    }
-  }
-  if (nonNitroWebhookMessage != null && message.deleted == false) {
     message.delete().catch((e) => console.log('Failed to delete a message: ', e));
+    sendDeleteLog(message, spamEmojiDeleteEmbed);
+    return;
   }
 };
