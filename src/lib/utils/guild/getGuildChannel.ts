542c7a928e8fc64955c02035f87f6977ae1ac5e3
Kazumik0108 <61595586+Kazumik0108@users.noreply.github.com>
2021-03-31 21:47:42 -0400

    guild channel util update
    The utility function getGuildChannel.ts now returns only GuildChannel | null, because they always had to be typecast to get the correct type.
    
    Now, typeguards in channelType.ts can be used to verify the type of GuildChannel.

--- src/lib/utils/guild/getGuildChannel.ts
+++ src/lib/utils/guild/getGuildChannel.ts
@@ -1,20 +1,6 @@
-import { CategoryChannel, Client, NewsChannel, StoreChannel, TextChannel, VoiceChannel } from 'discord.js';
+import { Guild } from 'discord.js';
 
-export const getGuildChannel = (id: string, client: Client) => {
-  const channel = client.channels.cache.get(id);
-  if (channel == undefined || channel.type == 'dm' || channel.type == 'unknown') return null;
-  switch (channel.type) {
-    case 'text':
-      return channel as TextChannel;
-    case 'voice':
-      return channel as VoiceChannel;
-    case 'category':
-      return channel as CategoryChannel;
-    case 'news':
-      return channel as NewsChannel;
-    case 'store':
-      return channel as StoreChannel;
-    default:
-      return null;
-  }
+export const getGuildChannel = (property: string, guild: Guild) => {
+  const channel = guild.channels.cache.find((e) => e.id == property || e.name == property);
+  return channel != undefined ? channel : null;
 };
